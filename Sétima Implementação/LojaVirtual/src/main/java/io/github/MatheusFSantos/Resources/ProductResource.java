9acb35d12b99a0ef35b1fdbd9aac8a4271fda38d
Matheus-FSantos <matheus.fs.contato@gmail.com>
2023-02-21 10:24:13 -0300

    Projeto - Sétima Implementação (Adição dos Relacionamentos entre colunas - ManyToMany)

---
+++ Sétima Implementação/LojaVirtual/src/main/java/io/github/MatheusFSantos/Resources/ProductResource.java
@@ -0,0 +1,33 @@
+package io.github.MatheusFSantos.Resources;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.MatheusFSantos.Entities.Product;
+import io.github.MatheusFSantos.Services.ProductService;
+
+@RestController
+@RequestMapping("/products")
+public class ProductResource {
+
+	@Autowired
+	private ProductService productService;
+	
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll(){
+		return ResponseEntity.ok().body(productService.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id){
+		return ResponseEntity.ok().body(productService.findById(id));
+	}
+}
